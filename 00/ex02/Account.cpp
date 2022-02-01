59f85da2b4fe3a218d1bf0dde377acd48a1b98b8
fncbarros <barrosfnc@gmail.com>
2022-02-01 11:26:57 +0000

    nearly done

--- 00/ex02/Account.cpp
+++ 00/ex02/Account.cpp
@@ -15,23 +15,40 @@
 # include <iomanip>
 # include <ctime>
 
+/*static Members*/
+
+int	Account::_nbAccounts; // need initialize?
+int	Account::_totalAmount;
+int	Account::_totalNbDeposits;
+int	Account::_totalNbWithdrawals;
+
 /*Constructors*/
 
 Account::Account( void ) {
-	_amount = 0;
-	_nbDeposits = 0;
+	this->_accountIndex = getNbAccounts();
+	this->_amount = 0;
+	this->_nbDeposits = 0;
+	this->_nbWithdrawals = 0;
 } ;
 
 Account::Account( int initial_deposit) {
 	// cout << "Constructor called." << endl;
+	Account();
 	this->_amount = initial_deposit; // ??
 } ;
 
-/*Account::Account( int initial_deposit): amount(initial_deposit) /*??*/
+/*Account::Account( int initial_deposit): amount(initial_deposit)*/ /*??*/
 /*{
 	// cout << "Constructor called." << endl;
 	this->_amount = initial_deposit;
 } ;*/
+
+/*Destructor*/
+
+Account::~Account( void ) {
+	_nbAccounts--;
+	_totalAmount -= _amount;
+} ;
 
 /*Functions members*/
 int	Account::getNbAccounts( void ) {
@@ -52,11 +69,11 @@ int	Account::getNbWithdrawals( void ) {
 } ;
 
 void	Account::displayAccountsInfos( void ) {
-	/*[19920104_091532] accounts:8;total:20049;deposits:0;withdrawals:0*/
+	/*[display_time] accounts:n;total:n;deposits:n;withdrawals:n*/
 	_displayTimestamp();
 	std::cout << " accounts:" << getNbAccounts(); // must be relative to specific object (class instance ??)
 	std::cout << ";total:" << getTotalAmount();
-//	std::cout << _totalNbWithdrawals;
+//	std::cout << _totalNbWithdrawals; // valid ?? <------------------- [!]
 	std::cout << ";deposits:" << getNbDeposits();
 	std::cout << ";withdrawals:" << getNbWithdrawals() << std::endl;
 } ;
@@ -68,9 +85,12 @@ void	Account::makeDeposit( int deposit ) {
 } ;
 
 bool	Account::makeWithdrawal( int withdrawal ) {
+	if (0 > (_amount - withdrawal))
+		return (false);
 	this->_amount -= withdrawal;
 	this->_totalAmount -= withdrawal;
 	this->_nbWithdrawals++;
+	return (true);
 } ;
 
 int		Account::checkAmount( void ) const {
@@ -84,7 +104,7 @@ void	Account::displayStatus ( void ) const {
 	_displayTimestamp();
 	std::cout << " index:" << _accountIndex;
 	std::cout << ";amount:" << _amount;
-	std::cout << ";";
+	std::cout << ";" << std::endl;
 } ;
 
 void	Account::_displayTimestamp( void ) {
