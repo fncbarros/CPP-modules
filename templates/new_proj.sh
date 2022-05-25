bae53b452bdbb579f1620ba8065898bedbd33cda
Francisco Barros <Francisco.Barros@criticaltechworks.com>
2022-05-25 18:08:00 +0100

    minor improvs

--- templates/new_proj.sh
+++ templates/new_proj.sh
@@ -4,21 +4,24 @@ name=$2
 name_upper="$(tr '[:lower:]' '[:upper:]' <<< ${name})"
 
 mkdir $new_folder/inc/
-cp Makefile Class.cpp $new_folder
-cp ./inc/Class.hpp $new_folder/inc/
+cp Makefile $new_folder
+cp Class.cpp $new_folder/$name.cpp
+cp ./inc/Class.hpp $new_folder/inc/$name.hpp
 cd $new_folder
-<< EOF >> main.cpp
-
-#include "Class.hpp"
-
-int main()
-{
-	return (0);
-}
-
+<< EOF >> main.cpp \
+\
+#include "Class.hpp"\
+\
+int main()\
+{\
+	return (0);\
+}\
+\
 EOF
 
-sed -i 's/Class/$name/g' Class.cpp
-sed -i 's/Class/$name/g' inc/Class.hpp
-sed -i 's/CLASS/$name_upper/g' inc/Class.hpp
+sed -i '{s/Class/'$name'/g}' $name.cpp
+sed -i '{s/Class/'$name'/g}' inc/$name.hpp
+sed -i '{s/CLASS/'$name_upper'/g}' inc/$name.hpp
 
+
+##idea: prompt for exe name number of classes and name them individually
