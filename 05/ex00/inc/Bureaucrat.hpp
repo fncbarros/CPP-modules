cf95a14ddb42b94ba29d12c7e6e8f6805269b12a
Francisco Barros <Francisco.Barros@criticaltechworks.com>
2022-07-11 12:00:34 +0100

    05 ex00

---
+++ 05/ex00/inc/Bureaucrat.hpp
@@ -0,0 +1,61 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Bureaucrat.hpp                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fbarros <fbarros@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/09 17:16:42 by fbarros           #+#    #+#             */
+/*   Updated: 2022/07/11 11:08:04 by fbarros          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+
+#pragma once
+#ifndef BUREAUCRAT_H
+#define BUREAUCRAT_H
+
+#include <string>
+#include <iostream>
+#include <exception>
+
+class Bureaucrat {
+public:
+	Bureaucrat(const std::string name, const unsigned int grade);
+	Bureaucrat();
+	Bureaucrat(const Bureaucrat& other);
+	Bureaucrat& operator=(const Bureaucrat& other);
+	~Bureaucrat();
+
+	class GradeTooHighException : public std::exception {
+		const char *what() const throw() {
+			return "Grade too high!";
+		}
+	};
+
+	class GradeTooLowException : public std::exception {
+		const char *what() const throw() {
+			return "Grade too low!";
+		}
+	};
+
+	std::string	getName();
+	unsigned int	getGrade();
+
+	// prefix
+	Bureaucrat&	operator++();
+	Bureaucrat&	operator--();
+	// postfix
+	Bureaucrat	operator++(int);
+	Bureaucrat	operator--(int);
+
+
+private:
+	const std::string _name;
+	unsigned int	_grade;
+
+};
+
+	std::ostream&	operator<<(std::ostream& out, Bureaucrat& bureaucrat);
+
+#endif /* BUREAUCRAT_H */
---
+++ 05/ex00/src/main.cpp
@@ -0,0 +1,48 @@
+
+#include "Bureaucrat.hpp"
+
+int main()
+{
+	Bureaucrat	b1("Reuben", 2);
+	Bureaucrat	b2("Scott", 149);
+	Bureaucrat	b52;
+
+	std::cout << ++b1 << std::endl;
+	std::cout << --b2 << std::endl;
+	std::cout << (b52 = b2) << std::endl;
+
+	try {
+		b1++;
+	} catch(std::exception& e) {
+		std::cerr << e.what() << std::endl;
+	}
+	try {
+		b2--;
+	} catch(std::exception& e) {
+		std::cerr << e.what() << std::endl;
+	}
+	try {
+		Bureaucrat	b3("Inácio", -1);
+	} catch (std::exception& e) {
+		std::cerr << e.what() << std::endl;
+	}
+	try {
+		Bureaucrat b3("Kevin", 75);
+		Bureaucrat b4 = b3;
+		std::cout << b3 << std::endl;
+		Bureaucrat b5;
+		std::cout << b5.getName() << std::endl;
+		std::cout << b5.getGrade() << std::endl;
+		b5 = b4;
+		std::cout << b5 << std::endl;
+	} catch (std::exception& e) {
+		std::cerr << e.what() << std::endl;
+	}
+	try {
+		Bureaucrat b5("Karina", 199);
+	} catch (std::exception& e) {
+		std::cerr << e.what() << std::endl;
+	}
+
+	return (0);
+}
