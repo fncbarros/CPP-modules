c69543d8a5f4345d11072be3f6ccb321015a3e0c
fbarros <fbarros@student.42lisboa.com>
2022-09-04 15:50:15 +0100

    07/ex00 done; need more tests

---
+++ 07/ex00/inc/templates.hpp
@@ -0,0 +1,24 @@
+
+#ifndef TEMPLATES_H
+#define TEMPLATES_H
+
+template <typename T>
+void    swap(T& a, T& b) {
+    T tmp;
+
+    tmp = a;
+    a = b;
+    b = tmp;
+}
+
+template <typename T>
+T    min(const T& a, const T& b) {
+    return a < b ? a : b;
+}
+
+template<typename T>
+T   max(const T& a, const T& b) {
+    return a > b ? a : b;
+}
+
+#endif //TEMPLATES_H
